cdd24107b3121da73747d0e2ecbc8b539be1fee6
Akash <akashmane12598@gmail.com>
2020-09-25 10:48:52 +0530

    [Akash]Add. USecase

--- Program.cs
+++ Program.cs
@@ -6,8 +6,22 @@ namespace Pracs2
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to employee attendance code");
-            
+            //Console.WriteLine("Welcome to employee attendance code");
+            Console.WriteLine("Enter 1 for present and 0 for absent");
+            Random random = new Random();
+            //int n = Convert.ToInt32(Console.ReadLine());
+            int n = random.Next(0, 2);
+            String att = "";
+            if (n == 1)
+            {
+                att += "Present";
+            }
+            else
+            {
+                att += "Absent";
+            }
+            Console.WriteLine(att);
+
         }
     }
 }
