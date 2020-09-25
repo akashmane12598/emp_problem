9da4e62f696a1118965b0fbc1df24d3aeba47e06
Akash <akashmane12598@gmail.com>
2020-09-25 10:45:06 +0530

    [Akash]Add. Welcome emp

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Pracs2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee attendance code");
+            
+        }
+    }
+}
